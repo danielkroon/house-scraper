312028e6ae8f42fdd595e2e65ed6eff643558687
Daniël Kroon <drg.kroon@gmail.com>
2019-05-16 13:52:53 +0200

    added firebase private key to .env for heroku

--- index.js
+++ index.js
@@ -8,10 +8,13 @@ require('dotenv').config()
 
 // firestore
 const admin = require('firebase-admin')
-const serviceAccount = require(process.env.FIREBASE_SERVICE_ACCOUNT_KEY_PATH)
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert({
+    "projectId": process.env.FIREBASE_PROJECT_ID,
+    "private_key": process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    "client_email": process.env.FIREBASE_CLIENT_EMAIL,
+  }),
   databaseURL: process.env.FIREBASE_DATABASE_URL
 })
 const db = admin.firestore()
